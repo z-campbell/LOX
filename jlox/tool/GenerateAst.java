4519fa4a83385f38e33198d43171fc1bed91f3b8
Zach Campbell <zcampbell@ddi.local>
2018-03-07 19:03:48 -0800

    Added GenerateAst.java to tool

---
+++ jlox/tool/GenerateAst.java
@@ -0,0 +1,14 @@
+package com.craftinginterpreters.tool;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.List;
+
+public class GenerateAst {
+    public static void main(String[] args) throws IOException {
+        if (args.length !=1) {
+            System.err.println("Usage: generate ast <output directory");
+        }
+    }
+}
